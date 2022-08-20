9e87b3f546a9a223401b041cbdbab497e6df12a7
satish_sonkar <satish_sonkar@external.mckinsey.com>
2022-08-20 10:20:08 +0530

    fix daily attendence issue

--- src/components/employee/DailyAttendence.js
+++ src/components/employee/DailyAttendence.js
@@ -13,6 +13,7 @@ export default function DailyAttendence() {
         month: 0,
         year: 0
     }
+
     const [employeeList, setEmployeeList] = useState([]);
     const [dailyAttendenceData, setDailyAttendenceData] = useState([]);
     const selectionTypeEnum = { all: 0, none: 1, invert: 2 };
@@ -31,7 +32,6 @@ export default function DailyAttendence() {
         let selectedDate = new Date(attendenceDate);
         let attDate = getSelectedDate(selectedDate);
         let employeeData = dailyAttendenceData.find(x => x.employeeId === employeeId);
-        console.log(employeeData);
         return employeeData[`day${attDate.selectedDay}`];
     }
 
@@ -66,8 +66,8 @@ export default function DailyAttendence() {
     const handleCheckSelection = (selectionType) => {
         if (typeof selectionType === undefined)
             return;
-            let selectedDate = new Date(attendenceDate);
-            let attDate = getSelectedDate(selectedDate);
+        let selectedDate = new Date(attendenceDate);
+        let attDate = getSelectedDate(selectedDate);
         let model = dailyAttendenceData;
         for (let i = 0; i < dailyAttendenceData.length; i++) {
             switch (selectionType) {
@@ -82,8 +82,10 @@ export default function DailyAttendence() {
                     break;
             }
         }
-        setDailyAttendenceData({ ...model });
+        setDailyAttendenceData(model);
+        setEmployeeList(common.cloneObject(employeeList));
     }
+
     useEffect(() => {
         Api.Get(apiUrls.employeeController.getAll + `?PageNo=1&PageSize=10000`)
             .then(res => {
@@ -92,21 +94,23 @@ export default function DailyAttendence() {
                 updateAttendenceData(data);
             })
     }, []);
+
     useEffect(() => {
         Api.Get(apiUrls.monthlyAttendenceController.getDailyAttendence + `?attendenceDate=${attendenceDate}`)
             .then(res => {
                 let selectedDate = new Date(attendenceDate);
-                let attDate=getSelectedDate(selectedDate);
-                let data=res.data;
-                let attData=dailyAttendenceData;
+                let attDate = getSelectedDate(selectedDate);
+                let data = res.data;
+                let attData = dailyAttendenceData;
                 data.forEach(element => {
-                    var record=attData.find(x=>x.employeeId===element.employeeId);
-                    record[`day${attDate.selectedDay}`]=element[`day${attDate.selectedDay}`];
+                    var record = attData.find(x => x.employeeId === element.employeeId);
+                    record[`day${attDate.selectedDay}`] = element[`day${attDate.selectedDay}`];
                 });
                 setDailyAttendenceData(attData);
                 setEmployeeList(common.cloneObject(employeeList));
             })
     }, [attendenceDate]);
+
     const attendenceChangeHandler = (e, employeeId) => {
         debugger;
         let data = dailyAttendenceData;
@@ -114,11 +118,11 @@ export default function DailyAttendence() {
         let attDate = getSelectedDate(selectedDate);
         let employeeData = data.find(x => x.employeeId === employeeId);
         employeeData[`day${attDate.selectedDay}`] = e.target.checked;
-        console.log(employeeData);
         setDailyAttendenceData(data);
         setEmployeeList(common.cloneObject(employeeList));
     }
-    const updateAttendenceData = (data, date) => {
+
+    const updateAttendenceData = (data, date, dayValue) => {
         let attendenceData = [];
         let selectedDate = new Date(date === undefined ? attendenceDate : date);
         let attDate = getSelectedDate(selectedDate);
@@ -126,11 +130,12 @@ export default function DailyAttendence() {
             dailyAttendenceModel.employeeId = element.id === undefined ? element.employeeId : element.id;
             dailyAttendenceModel.month = attDate.selectedMonth;
             dailyAttendenceModel.year = attDate.selectedYear;
-            dailyAttendenceModel[`day${attDate.selectedDay}`] = true;
+            dailyAttendenceModel[`day${attDate.selectedDay}`] = dayValue === undefined ? true : dayValue;
             attendenceData.push(common.cloneObject(dailyAttendenceModel));
         });
         setDailyAttendenceData(attendenceData);
     }
+
     const getSelectedDate = (date) => {
         return {
             selectedMonth: date.getMonth() + 1,
@@ -138,6 +143,7 @@ export default function DailyAttendence() {
             selectedDay: date.getDate()
         }
     }
+
     const saveAttendence = () => {
         Api.Put(apiUrls.monthlyAttendenceController.addUpdateDailyAttendence, dailyAttendenceData)
             .then(res => {
@@ -146,53 +152,54 @@ export default function DailyAttendence() {
                 }
             })
     }
-    return (<>
-        <Breadcrumb option={breadcrumbOption} />
-        <h6 className="mb-0 text-uppercase">Employee Daily Attendence</h6>
-        <div className="col-12 col-md-12">
-            <div className='row'>
-                <div className='col-2'>  <h6 className="mb-0 text-uppercase"></h6></div>
-                <div className='col-10' style={{ textAlign: 'right' }}>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" name='chkSelection' onChange={e => handleCheckSelection(e.target.checked ? selectionTypeEnum.all : selectionTypeEnum.none)} type="checkbox" id="gridCheck2" />
-                        <label className="form-check-label" htmlFor="gridCheck2">Select All</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" onChange={e => handleCheckSelection(selectionTypeEnum.invert)} type="checkbox" id="gridCheck2" />
-                        <label className="form-check-label" name="chkSelection" htmlFor="gridCheck2">Invert Selection</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <label className="form-check-label" htmlFor="attendenceDate">Attendence Date</label>
-                        <input className="form-control-sm" min={common.getHtmlDate(common.getFirstDateOfMonth())} max={common.getHtmlDate(new Date())} name='attendenceDate' value={attendenceDate} onChange={e => attendenceDateChangeHandler(e)} type="date" id="attendenceDate" />
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <button className='btn btn-sm btn-success' onClick={e => saveAttendence()} ><i className="bi bi-cloud-arrow-up"></i> Save</button>
+
+    return (
+        <>
+            <Breadcrumb option={breadcrumbOption} />
+            <h6 className="mb-0 text-uppercase">Employee Daily Attendence</h6>
+            <div className="col-12 col-md-12">
+                <div className='row'>
+                    <div className='col-2'>  <h6 className="mb-0 text-uppercase"></h6></div>
+                    <div className='col-10' style={{ textAlign: 'right' }}>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" name='chkSelection' onClick={e => handleCheckSelection(e.target.checked ? selectionTypeEnum.all : selectionTypeEnum.none)} type="radio" id="gridCheck2" />
+                            <label className="form-check-label" htmlFor="gridCheck2">Select All</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" name="chkSelection" onClick={e => handleCheckSelection(selectionTypeEnum.invert)} type="radio" id="gridCheck2" />
+                            <label className="form-check-label" name="chkSelection" htmlFor="gridCheck2">Invert Selection</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <label className="form-check-label" htmlFor="attendenceDate">Attendence Date</label>
+                            <input className="form-control-sm" min={common.getHtmlDate(common.getFirstDateOfMonth())} max={common.getHtmlDate(new Date())} name='attendenceDate' value={attendenceDate} onChange={e => attendenceDateChangeHandler(e)} type="date" id="attendenceDate" />
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <button className='btn btn-sm btn-success' onClick={e => saveAttendence()} ><i className="bi bi-cloud-arrow-up"></i> Save</button>
+                        </div>
                     </div>
                 </div>
+                <hr />
             </div>
-
-            <hr />
-        </div>
-        <table className="table table-striped table-bordered dataTable">
-            <tbody>
-                {
-                    employeeList?.map((ele, index) => {
-                        return <tr key={ele.id}>
-                            <td>{ele.firstName + "  " + ele.lastName}</td>
-                            <td>{ele.contact}</td>
-                            <td>{ele.jobTitle}</td>
-                            <td>
-                                <div className="form-check form-switch">
-                                    <input onChange={e => attendenceChangeHandler(e, ele.id)} checked={getEmployeeAttendence(ele.id) ? "checked" : ""} className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
-                                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
-                                    </label>
-                                </div>
-                            </td>
-                        </tr>
-                    })
-                }
-            </tbody>
-        </table>
-    </>
+            <table className="table table-striped table-bordered dataTable">
+                <tbody>
+                    {
+                        employeeList?.map((ele, index) => {
+                            return <tr key={ele.id}>
+                                <td>{ele.firstName + "  " + ele.lastName}</td>
+                                <td>{ele.contact}</td>
+                                <td>{ele.jobTitle}</td>
+                                <td>
+                                    <div className="form-check form-switch">
+                                        <input onChange={e => attendenceChangeHandler(e, ele.id)} checked={getEmployeeAttendence(ele.id) ? "checked" : ""} className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+                                        </label>
+                                    </div>
+                                </td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
