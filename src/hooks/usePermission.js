f0d8c1a16ab9d80e4b72896ebbbcead998107051
Satish Sonkar <satish.sonkar@nagarro.com>
2022-11-01 07:38:34 +0530

    fix issue

--- src/hooks/usePermission.js
+++ src/hooks/usePermission.js
@@ -3,6 +3,8 @@ import useLocalStorage from './useLocalStorage';
 export default function usePermission() {
     const [getItem] = useLocalStorage(process.env.REACT_APP_PERMISSION_STORAGE_KEY);
     var permissions = getItem();
+    if(!permissions)
+    return;
     const hasUserPermission = (permissionName) => {
         debugger;
         return permissions.find(x => x.permissionResourceCode.toLowerCase() === permissionName.toLowerCase()) === undefined ? false : true;
