8a20d61f4eb97804cc79622f337e8437053ed268
Satish Sonkar <btech.csit@gmail.com.com>
2023-04-07 18:57:15 +0530

    update

--- src/components/print/admin/account/PrintBillingTaxReport.js
+++ src/components/print/admin/account/PrintBillingTaxReport.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { common } from '../../../../utils/common'
 import BillingTaxTable from '../../../account/BillingTaxTable'
 import InvoiceHead from '../../../common/InvoiceHead'
 
@@ -7,12 +8,8 @@ export const PrintBillingTaxReport = React.forwardRef((props, ref) => {
     return (
         <div ref={ref} className="p-3">
             <InvoiceHead receiptType='Billing Tax Report'></InvoiceHead>
-            <div className='card'>
-                <div className='card-body'>
-                    <div className='text-end'>Report from {props.props.filter.fromDate} To {props.props.filter.toDate}</div>
-                    <BillingTaxTable billingData={props.props?.data}/>
-                </div>
-            </div>
+            <div className='text-end my-3'>Report from {common.getHtmlDate(new Date(props.props.filter.fromDate),"ddmmyyyy")} To {common.getHtmlDate(new Date(props.props.filter.toDate),"ddmmyyyy")}</div>
+            <BillingTaxTable forReport={true} billingData={props.props?.data} showBalanceVat={false} showPrintOption={false} />
         </div>
     )
 })
