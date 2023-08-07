428183f80a0426e1a99be7b6b4b7810c6206db20
SATISH KUMAR SONKER <satishkumar.sonker@ihsmarkit.com>
2023-08-07 12:02:45 +0530

    Update Company Name

--- src/components/common/InvoiceHead.js
+++ src/components/common/InvoiceHead.js
@@ -15,13 +15,13 @@ export default function InvoiceHead({ receiptType = "TAX INVOICE", hideTrnNo = f
             </div>
             <div className='col-4 py-2'>
                 <div className='text-center'>
-                    <img style={{ width: '39%', height: '100px' }} src={process.env.REACT_APP_LOGO} alt='La Beach Logo' />
+                    <img style={{ width: '39%', height: '100px' }} src={process.env.REACT_APP_LOGO} alt='SALEH GHAREEB TAILORING TEXTILE Logo' />
                     <div className='text-center text-uppercase' style={{ fontSize: fontSize }}>{receiptType}</div>
                     {!hideTrnNo && <div className='text-center' style={{ fontSize: fontSize, padding: '4px', borderRadius: '1000px', border: '1px solid black' }} >TRN : 100230766600003</div>}
                 </div>
             </div>
             <div className='col-4 py-2'>
-                <div className='text-end fw-bold' style={{ fontSize: '13px' }}>لا بيخ الخياطة والتطويز</div>
+                <div className='text-end fw-bold' style={{ fontSize: '13px' }}>صالح غريب للخياطة المنسوجات</div>
                 <div className='text-end' style={{ fontSize: fontSize }}>ثلاجة مرار</div>
                 <div className='text-end' style={{ fontSize: fontSize }}>بالقرب من مسجد الفطيم</div>
                 <div className='text-end' style={{ fontSize: fontSize }}>ص. ب : ٦١٩٧٥</div>
