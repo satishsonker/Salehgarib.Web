f9eb0eb003a3d15fb4c47a19963e2ed1dae5f536
SATISH KUMAR SONKER <satishkumar.sonker@ihsmarkit.com>
2023-10-04 20:42:47 +0530

    fix

--- src/components/common/InvoiceHead.js
+++ src/components/common/InvoiceHead.js
@@ -5,7 +5,8 @@ export default function InvoiceHead({ receiptType = "TAX INVOICE", hideTrnNo = f
     return (
         <div className='row '>
             <div className='col-4 py-2'>
-                <div className='text-start fw-bold' style={{ fontSize: '13px' }}>{process.env.REACT_APP_COMPANY_NAME}</div>
+                <div className='text-start fw-bold' style={{ fontSize: '22px' }}>{process.env.REACT_APP_COMPANY_NAME}</div>
+                <div className='text-start fw-bold' style={{ fontSize: fontSize }}>{process.env.REACT_APP_COMPANY_SUBNAME}</div>
                 <div className='text-start' style={{ fontSize: fontSize }}>Al Mushrif</div>
                 <div className='text-start' style={{ fontSize: fontSize }}>Near Immigration Bridge</div>
                 <div className='text-start' style={{ fontSize: fontSize }}>Old Airport Road</div>
@@ -16,14 +17,15 @@ export default function InvoiceHead({ receiptType = "TAX INVOICE", hideTrnNo = f
             </div>
             <div className='col-4 py-2'>
                 <div className='text-center'>
-                    <img style={{ width: '39%', height: '100px' }} src={process.env.REACT_APP_LOGO} alt='SALEH GHAREEB TAILORING TEXTILE Logo' />
+                    <img style={{ width: '39%', height: '100px' }} src={process.env.REACT_APP_LOGO} alt='SALEH GHAREEB TAILORING TEXTILE BR. Logo' />
                     <div className='text-center text-uppercase' style={{ fontSize: fontSize }}>{receiptType}</div>
                     {!hideTrnNo && <div className='text-center' style={{ fontSize: fontSize, padding: '4px', borderRadius: '1000px', border: '1px solid black' }} >TRN : 104107669400003</div>}
                 </div>
             </div>
             <div className='col-4 py-2'>
-                <div className='text-end fw-bold' style={{ fontSize: '13px' }}>صالح غريب للخياطة المنسوجات</div>
-                <div className='text-end fw-bold' style={{ fontSize: fontSize }}>المشرف</div>
+                <div className='text-end fw-bold' style={{ fontSize: '22px' }}>صالح غريب</div> 
+                <div className='text-end fw-bold' style={{ fontSize: fontSize}}>خياطة المنسوجات - فرع</div>
+                <div className='text-end' style={{ fontSize: fontSize }}>المشرف</div>
                 <div className='text-end' style={{ fontSize: fontSize }}>بالقرب من جسر الهجرة</div>
                 <div className='text-end' style={{ fontSize: fontSize }}>طريق المطار القديم</div>
                 <div className='text-end' style={{ fontSize: fontSize }}>ص. ب : ۷٥۰۳۸</div>
