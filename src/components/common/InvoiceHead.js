dc275946f7cc8fef34d114560d6bb7581162dba4
Satish Kumar Sonker <btech.csit@gmail.com>
2024-09-12 21:19:23 +0530

    chage comapny name

--- src/components/common/InvoiceHead.js
+++ src/components/common/InvoiceHead.js
@@ -8,29 +8,29 @@ export default function InvoiceHead({ receiptType = "TAX INVOICE", hideTrnNo = f
             <div className='col-4 py-2'>
                 <div className='text-start fw-bold' style={{ fontSize: companyNameFontSize }}>{process.env.REACT_APP_COMPANY_NAME}</div>
                 <div className='text-start fw-bold' style={{ fontSize: fontSize }}>{process.env.REACT_APP_COMPANY_SUBNAME}</div>
-                <div className='text-start' style={{ fontSize: fontSize }}>Near Immigration Bridge</div>
-                <div className='text-start' style={{ fontSize: fontSize }}>Old Airport Road</div>
-                <div className='text-start' style={{ fontSize: fontSize }}>P.O. Box : 75038</div>
-                <div className='text-start' style={{ fontSize: fontSize }}>Abu Dhabi - U.A.E</div>
-                <div className='text-start' style={{ fontSize: fontSize }}>Tel : 02-4436530</div>
+                <div className='text-start' style={{ fontSize: fontSize }}>Frij Al Murar</div>
+                <div className='text-start' style={{ fontSize: fontSize }}>Near Al futtaim Masjid</div>
+                <div className='text-start' style={{ fontSize: fontSize }}>P.O. Box : 61975</div>
+                <div className='text-start' style={{ fontSize: fontSize }}>Dubai - U.A.E</div>
+                <div className='text-start' style={{ fontSize: fontSize }}>Tel : {process.env.REACT_APP_COMPANY_NUMBER}</div>
                 <div className='text-start' style={{ fontSize: fontSize }}>Mobile : {process.env.REACT_APP_COMPANY_MOBILE}</div>
             </div>
             <div className='col-4 py-2'>
                 <div className='text-center'>
-                    <img style={{ width: '39%', height: '100px' }} src={process.env.REACT_APP_LOGO} alt='SALEH GHAREEB TAILORING TEXTILE Logo' />
+                    <img style={{ width: '39%', height: '100px' }} src={process.env.REACT_APP_LOGO} alt='SALEH GHAREEB TAILORING TEXTILE DUBAI Logo' />
                     <div className='text-center text-uppercase' style={{ fontSize: fontSize }}>{receiptType}</div>
-                    {!hideTrnNo && <div className='text-center' style={{ fontSize: fontSize, padding: '4px', borderRadius: '1000px', border: '1px solid black' }} >TRN : 104107669400003</div>}
+                    {!hideTrnNo && <div className='text-center' style={{ fontSize: fontSize, padding: '4px', borderRadius: '1000px', border: '1px solid black' }} >TRN : 100230766600003</div>}
                 </div>
             </div>
             <div className='col-4 py-2'>
                 <div className='text-end fw-bold' style={{ fontSize: companyNameFontSize }}>صالح غريب</div>
-                <div className='text-end fw-bold' style={{ fontSize: fontSize }}>خياطة المنسوجات - دبي</div>
-                <div className='text-end' style={{ fontSize: fontSize }}>بالقرب من جسر الهجرة</div>
-                <div className='text-end' style={{ fontSize: fontSize }}>طريق المطار القديم</div>
-                <div className='text-end' style={{ fontSize: fontSize }}>ص. ب : ۷٥۰۳۸</div>
-                <div className='text-end' style={{ fontSize: fontSize }}>أبو ظبي - الإمارات العربية المتحدة</div>
-                <div className='text-end' style={{ fontSize: fontSize }}>هاتف : ۰۲-٤٤۳٦٥۳۰</div>
-                <div className='text-end' style={{ fontSize: fontSize }}>محمول : +۹۷۱٥٦۷۸۰۰۱٦٥</div>
+                <div className='text-end fw-bold' style={{ fontSize: fontSize }}>خياطة المنسوجات – دبي</div>
+                <div className='text-end' style={{ fontSize: fontSize }}>فريج المرار</div>
+                <div className='text-end' style={{ fontSize: fontSize }}>بالقرب من مسجد الفطيم </div>
+                <div className='text-end' style={{ fontSize: fontSize }}>ص.ب. صندوق : ٦۱۹۷٥</div>
+                <div className='text-end' style={{ fontSize: fontSize }}>دبي - الإمارات العربية المتحدة</div>
+                <div className='text-end' style={{ fontSize: fontSize }}>هاتف : ۰٤-۲۷۲۱۳٤۲</div>
+                <div className='text-end' style={{ fontSize: fontSize }}>+جوال : ۹۷۱٥٥٤٦۸۰۰۲۲</div>
             </div>
         </div>
     )
