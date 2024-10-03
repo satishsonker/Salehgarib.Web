805a324f99ee2252bf56f92830c665c0ea9234b1
Satish Kumar Sonker <btech.csit@gmail.com>
2024-10-04 00:04:13 +0530

    La beach

--- src/components/print/PrintWorkerSheet.js
+++ src/components/print/PrintWorkerSheet.js
@@ -5,7 +5,7 @@ import { common } from '../../utils/common';
 import ButtonBox from '../common/ButtonBox'
 import ReactToPrint from 'react-to-print';
 
-export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,refreshData }) {
+export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex, refreshData }) {
     const [modelImages, setModelImages] = useState([]);
     const [mainData, setMainData] = useState(common.cloneObject(orderData));
     const printRef = useRef();
@@ -21,27 +21,27 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
             .then(res => {
                 setModelImages(res.data);
             })
-    }, [orderData,pageIndex]);
-    
+    }, [orderData, pageIndex]);
+
     useEffect(() => {
-        Api.Get(apiUrls.orderController.get+`${orderData.id}`)
-        .then(res=>{
-            if(res.data?.id>0)
-            setMainData(res.data);
-        })
+        Api.Get(apiUrls.orderController.get + `${orderData.id}`)
+            .then(res => {
+                if (res.data?.id > 0)
+                    setMainData(res.data);
+            })
     }, [refreshData])
 
     if (orderData === undefined || orderData.orderNo === undefined) {
         return <></>
     }
-    
+
     const getModelImage = (id) => {
         var imagePath = modelImages.find(x => x.moduleId === id);
 
         if (imagePath) {
             console.log(imagePath.filePath, 'photo');
             console.log(process.env.REACT_APP_API_URL, 'photo');
-        return imagePath.filePath;
+            return imagePath.filePath;
         }
         return "";
     }
@@ -54,6 +54,17 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
                         return <button className='btn btn-sm btn-warning' data-bs-dismiss="modal"><i className='bi bi-printer'></i> Print Work Sheet</button>
                     }}
                     content={(el) => (printRef.current)}
+                    pageStyle={`
+                        @media print {
+                          @page {
+                            size: A4;
+                            margin:5mm; /* Customize page margins */
+                          }
+                          .print-page {
+                            page-break-after: always; /* Ensure each page breaks after the third item */
+                          }
+                        }
+                      `}
                 />
             </div>
             <div ref={printRef} style={{ padding: '10px', fontSize: '10px' }} className="row">
@@ -61,14 +72,17 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
                 {
                     mainData?.orderDetails?.map((ele, index) => {
                         return <>
-                            <div key={ele.id} className="col-9" style={{ height: '386px', maxHeight: '386px' }}>
+                            {index % 3 === 0 && index !== 0 && (
+                                <div className="print-page"></div> /* Add a break after every 3 items */
+                            )}
+                            <div key={ele.id} className="col-9" style={{ height: '370px', maxHeight: '370px' }}>
                                 <div className="card shadow-none">
                                     <div className='row'>
                                         <div className='col-12'>
                                             <table className='table table-bordered'>
                                                 <tbody>
                                                     <tr>
-                                                        <td colSpan={6} className="text-center"> {process.env.REACT_APP_COMPANY_NAME}</td>
+                                                        <td colSpan={6} className="text-center"> {process.env.REACT_APP_COMPANY_NAME} {process.env.REACT_APP_COMPANY_SUBNAME}</td>
                                                     </tr>
                                                     <tr>
                                                         <td colSpan={2} className=" text-center fw-bold fs-6">Worker Sheet</td>
@@ -82,18 +96,14 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
                                                         <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{mainData.salesman.split(' ')[0].trim()}</td>
                                                         <td style={{ padding: '0 0 0 8px' }}>D. Date</td>
                                                         <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{common.getHtmlDate(mainData.orderDeliveryDate, 'ddmmyyyy')}</td>
-                                                        {/* <td style={{ padding: '0 0 0 8px' }}>Grade</td>
-                                                        <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{common.getGrade(ele.totalAmount)}</td> */}
-                                                        {/* <td style={{ padding: '0 0 0 8px' }}>Model</td>
-                                                        <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{ele.modelName}</td> */}
                                                     </tr>
                                                     <tr>
                                                         <td style={{ padding: '0 0 0 8px' }}>Qty</td>
                                                         <td style={{ padding: '0 0 0 8px', width: '80px' }} className=" fw-bold">{mainData.orderDetails.length}</td>
                                                         <td style={{ padding: '0 0 0 8px' }}>Grade</td>
                                                         <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{common.getGrade(ele?.subTotalAmount)}</td>
-                                                       <td style={{ padding: '0 0 0 8px' }}>Name</td>
-                                                        <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{ele.measurementCustomerName===null || ele.measurementCustomerName===""?mainData.customerName.split('-')[0].trim():ele.measurementCustomerName}</td>
+                                                        <td style={{ padding: '0 0 0 8px' }}>Name</td>
+                                                        <td style={{ padding: '0 0 0 8px' }} className=" fw-bold">{ele.measurementCustomerName === null || ele.measurementCustomerName === "" ? mainData.customerName.split('-')[0].trim() : ele.measurementCustomerName}</td>
                                                     </tr>
                                                     <tr>
                                                         <td style={{ padding: '0 0 0 8px' }} className='text-uppercase'>customer name</td>
@@ -113,9 +123,9 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
                                                         <td style={{ padding: '15px' }}>Hand. Emb.</td>
                                                         <td style={{ padding: '15px' }}></td>
                                                         <td style={{ padding: '15px' }} rowSpan={5} colSpan={2}>
-                                                            <img style={{ display: 'block', width: '100%', maxHeight: '96px' }} 
-                                                            onError={(e)=>{e.target.src="/assets/images/default-image.jpg"}} 
-                                                            src={process.env.REACT_APP_API_URL + getModelImage(ele.designSampleId)} ></img>
+                                                            <img style={{ display: 'block', width: '100%', maxHeight: '96px' }}
+                                                                onError={(e) => { e.target.src = "/assets/images/default-image.jpg" }}
+                                                                src={process.env.REACT_APP_API_URL + getModelImage(ele.designSampleId)} ></img>
                                                         </td>
                                                     </tr>
                                                     <tr>
@@ -131,7 +141,7 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
                                                         <td style={{ padding: '15px' }}></td>
                                                     </tr>
                                                     <tr>
-                                                        <td rowSpan={2} colSpan={4}>.<br />.<br />Check and updated by............<br />.<br />.</td>
+                                                        <td rowSpan={2} colSpan={4}>.<br />Check and updated by............</td>
                                                     </tr>
                                                 </tbody>
                                             </table>
@@ -152,7 +162,7 @@ export default function PrintWorkerSheet({ orderData, pageIndex, setPageIndex,re
                                             </tr>
                                             <tr>
                                                 <td style={{ padding: '0 0 0 5px' }}>Customer</td>
-                                                <td style={{ padding: '0px' }} className="text-center">{ele.measurementCustomerName===null || ele.measurementCustomerName===""?mainData.customerName.split('-')[0].trim():ele.measurementCustomerName}</td>
+                                                <td style={{ padding: '0px' }} className="text-center">{ele.measurementCustomerName === null || ele.measurementCustomerName === "" ? mainData.customerName.split('-')[0].trim() : ele.measurementCustomerName}</td>
                                             </tr>
                                             <tr>
                                                 <td style={{ padding: '0 0 0 5px' }}>Grade</td>
