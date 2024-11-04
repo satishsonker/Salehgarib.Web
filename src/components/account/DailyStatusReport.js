c41e6785d5ae100dc878e3b318af3fde20699ea2
Satish Kumar Sonker <btech.csit@gmail.com>
2024-11-05 00:56:38 +0530

    fix daily status issue

--- src/components/account/DailyStatusReport.js
+++ src/components/account/DailyStatusReport.js
@@ -1,8 +1,8 @@
-import React, { useState, useRef, useMemo } from 'react'
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 import { Api } from '../../apis/Api';
 import { apiUrls } from '../../apis/ApiUrls';
-import { common } from '../../utils/common'
-import Breadcrumb from '../common/Breadcrumb'
+import { common } from '../../utils/common';
+import Breadcrumb from '../common/Breadcrumb';
 import ButtonBox from '../common/ButtonBox';
 import Inputbox from '../common/Inputbox';
 import { useReactToPrint } from 'react-to-print';
@@ -16,27 +16,28 @@ export default function DailyStatusReport() {
     const VAT = parseFloat(process.env.REACT_APP_VAT);
     const printStatusReportRef = useRef();
 
-    const getStatusData = async () => {
-        const res = await Api.Get(`${apiUrls.reportController.getDailyStatusReport}${statusDate}`);
-        setStatusData(res.data);
-    };
+    const getStatusData = useCallback(async () => {
+        try {
+            const res = await Api.Get(`${apiUrls.reportController.getDailyStatusReport}${statusDate}`);
+            setStatusData(res.data);
+        } catch (error) {
+            console.error('Error fetching status data:', error);
+        }
+    }, [statusDate]);
 
     const printStatusHandler = useReactToPrint({
         content: () => printStatusReportRef.current,
     });
 
-    // Filtered data based on payment mode
     const filteredAccountStatements = useMemo(() => (
         paymentModeFilter === 'All'
             ? statusData.customerAccountStatements
             : statusData.customerAccountStatements?.filter(item => item.paymentMode === paymentModeFilter)
     ), [statusData.customerAccountStatements, paymentModeFilter]);
 
-    // Extract unique payment modes from data for radio buttons
-    const paymentModes = useMemo(() => {
-        const modes = ['All', ...new Set(statusData.customerAccountStatements?.map(item => item.paymentMode))];
-        return modes.filter(mode => mode); // Remove any undefined values
-    }, [statusData.customerAccountStatements]);
+    const paymentModes = useMemo(() => (
+        ['All', ...new Set(statusData.customerAccountStatements?.map(item => item.paymentMode))].filter(Boolean)
+    ), [statusData.customerAccountStatements]);
 
     const breadcrumbOption = useMemo(() => ({
         title: 'Daily Status Report',
@@ -51,49 +52,44 @@ export default function DailyStatusReport() {
         { type: 'Print', onClickHandler: printStatusHandler, className: 'btn-sm' }
     ], [getStatusData, printStatusHandler]);
 
-    const totalBookingQty = useMemo(() => {
-        return statusData?.orders?.filter(x => paymentModeFilter==='All'||x.paymentMode === paymentModeFilter)?.reduce((sum, ele) => sum + ele?.qty, 0) || 0
-    }, [statusData.orders, paymentModeFilter]);
+    const calculateTotal = (data, filterCondition, key) => {
+        debugger;
+        var res = data?.filter(filterCondition).reduce((sum, item) => sum + item[key], 0) || 0
+        return res;
+    }
+        ;
 
-    const totalBookingAmount = useMemo(() => (
-        common.printDecimal(statusData.orders?.filter(x =>paymentModeFilter==='All'|| x.paymentMode === paymentModeFilter)?.reduce((sum, ele) => sum + ele.totalAmount, 0) || 0)
+    const totalBookingQty = useMemo(() => (
+        calculateTotal(statusData.orders, x => paymentModeFilter === 'All' || x.paymentMode === paymentModeFilter, 'qty')
     ), [statusData.orders, paymentModeFilter]);
 
-    const totalBookingAdvanceCash = useMemo(() => (
-        common.printDecimal(filteredAccountStatements?.reduce((sum, ele) =>
-            (paymentModeFilter === 'All' ||(ele.paymentMode?.toLowerCase() === 'cash' && paymentModeFilter?.toLowerCase() === 'cash')) && ele.reason === "AdvancedPaid" ? sum + ele.credit : sum
-            , 0) || 0)
-    ), [filteredAccountStatements, paymentModeFilter]);
+    const totalBookingAmount = useMemo(() => (
+        common.printDecimal(calculateTotal(statusData.orders, x => paymentModeFilter === 'All' || x.paymentMode === paymentModeFilter, 'totalAmount'))
+    ), [statusData.orders, paymentModeFilter]);
 
-    const totalBookingAdvanceVisa = useMemo(() => (
+    const totalAdvanceAmount = useCallback((mode) => (
         common.printDecimal(filteredAccountStatements?.reduce((sum, ele) =>
-            (paymentModeFilter === 'All' || (ele.paymentMode?.toLowerCase() === 'visa' && paymentModeFilter?.toLowerCase() === 'visa')) && ele.reason === "AdvancedPaid" ? sum + ele.credit : sum
+            (ele.paymentMode?.toLowerCase() ===mode && (paymentModeFilter?.toLowerCase()===mode || paymentModeFilter==='All')) && ele.reason === "AdvancedPaid" ? sum + ele.credit : sum
             , 0) || 0)
     ), [filteredAccountStatements, paymentModeFilter]);
 
     const totalDeliveredQty = useMemo(() => (
-        filteredAccountStatements?.reduce((sum, ele) => (paymentModeFilter === 'All' ||ele.paymentMode?.toLowerCase() === paymentModeFilter?.toLowerCase()) ? sum + ele.deliveredQty : sum, 0) || 0
+        calculateTotal(filteredAccountStatements, ele => paymentModeFilter === 'All' || ele.paymentMode?.toLowerCase() === paymentModeFilter?.toLowerCase(), 'deliveredQty')
     ), [filteredAccountStatements, paymentModeFilter]);
 
-    const totalDeliveryCash = useMemo(() => (
-        common.printDecimal(filteredAccountStatements?.reduce((sum, ele) =>
-            (paymentModeFilter === 'All' ||(ele.paymentMode?.toLowerCase() === 'cash' && paymentModeFilter?.toLowerCase() === 'cash')) && ele.reason?.toLowerCase() === 'paymentreceived' ? sum + ele.credit : sum
-            , 0) || 0)
-    ), [filteredAccountStatements]);
-
-    const totalDeliveryVisa = useMemo(() => (
-        common.printDecimal(filteredAccountStatements?.reduce((sum, ele) =>
-            (paymentModeFilter === 'All' ||(ele.paymentMode?.toLowerCase() === 'visa' && paymentModeFilter?.toLowerCase() === 'visa')) && ele.reason?.toLowerCase() === 'paymentreceived' ? sum + ele.credit : sum
-            , 0) || 0)
+    const totalDeliveryAmount = useCallback((mode) => (
+        common.printDecimal(filteredAccountStatements?.reduce((sum, ele) => {
+            return ele.paymentMode?.toLowerCase() === mode && (paymentModeFilter?.toLowerCase() === mode || paymentModeFilter === 'All') && ele.reason?.toLowerCase() === 'paymentreceived' ? sum + ele.credit : sum
+        }, 0) || 0)
     ), [filteredAccountStatements, paymentModeFilter]);
 
     const totalSalesAmount = useMemo(() => (
-        filteredAccountStatements?.reduce((sum, ele) => (paymentModeFilter === 'All' || paymentModeFilter?.toLowerCase() === ele?.paymentMode?.toLowerCase()) ? sum + ele.credit : 0, 0) || 0
+        calculateTotal(filteredAccountStatements, ele => paymentModeFilter === 'All' || paymentModeFilter?.toLowerCase() === ele.paymentMode?.toLowerCase(), 'credit')
     ), [filteredAccountStatements, paymentModeFilter]);
 
     const totalVatTax = useMemo(() => (
         common.printDecimal(totalSalesAmount - common.calculatePercent(totalSalesAmount, 95))
-    ), [totalSalesAmount, paymentModeFilter]);
+    ), [totalSalesAmount]);
 
     const totalExpenseAmount = useMemo(() => (
         common.printDecimal(statusData.expenseAmount || 0)
@@ -104,20 +100,18 @@ export default function DailyStatusReport() {
             <Breadcrumb option={breadcrumbOption} />
             <div className="d-flex justify-content-between">
                 <h6 className="mb-0 text-uppercase">Daily Status Report</h6>
-                <div>
-                    <div className="d-flex">
-                        <Inputbox
-                            title="From Date"
-                            max={common.getHtmlDate(new Date())}
-                            onChangeHandler={e => setStatusDate(e.target.value)}
-                            name="fromDate"
-                            value={statusDate}
-                            className="form-control-sm"
-                            showLabel={false}
-                            type="date"
-                        />
-                        <ButtonBox btnList={btnList} />
-                    </div>
+                <div className="d-flex">
+                    <Inputbox
+                        title="From Date"
+                        max={common.getHtmlDate(new Date())}
+                        onChangeHandler={e => setStatusDate(e.target.value)}
+                        name="fromDate"
+                        value={statusDate}
+                        className="form-control-sm"
+                        showLabel={false}
+                        type="date"
+                    />
+                    <ButtonBox btnList={btnList} />
                 </div>
             </div>
             <hr />
@@ -154,17 +148,15 @@ export default function DailyStatusReport() {
                                 {filteredAccountStatements?.length > 0 ? (
                                     filteredAccountStatements.map((res, index) => (
                                         <tr key={index} style={{ fontSize: '12px' }}>
-                                            <td className="text-center" style={{ padding: '5px' }}>{index + 1}</td>
-                                            <td className="text-center" style={{ padding: '5px' }}>{res?.order?.orderNo}</td>
-                                            <td className="text-center" style={{ padding: '5px' }}>
-                                                {common.printDecimal(res.isFirstAdvance ? res.order.totalAmount : ((res.balance || 0) + (res.credit || 0)))}
-                                            </td>
-                                            <td className="text-center" style={{ padding: '5px' }}>{(res.deliveredQty || 0)}/{res?.order?.qty}</td>
-                                            <td className="text-center" style={{ padding: '5px' }}>{common.printDecimal(res.credit)}</td>
-                                            <td className="text-center" style={{ padding: '5px' }}>{common.printDecimal(res.balance)}</td>
-                                            <td className="text-center" style={{ padding: '5px' }}>{res.paymentMode}</td>
-                                            <td className="text-center" style={{ padding: '5px' }}>
-                                                {res?.isFirstAdvance ? "Booking Advance" : (res?.reason?.toLowerCase() === "advancedpaid" ? "Advance" : "Delivery")}
+                                            <td className="text-center">{index + 1}</td>
+                                            <td className="text-center">{res?.order?.orderNo}</td>
+                                            <td className="text-center">{common.printDecimal(res.isFirstAdvance ? res.order.totalAmount : ((res.balance || 0) + (res.credit || 0)))}</td>
+                                            <td className="text-center">{(res.deliveredQty || 0)}/{res?.order?.qty}</td>
+                                            <td className="text-center">{common.printDecimal(res.credit)}</td>
+                                            <td className="text-center">{common.printDecimal(res.balance)}</td>
+                                            <td className="text-center">{res.paymentMode}</td>
+                                            <td className="text-center">
+                                                {res.isFirstAdvance ? "Booking Advance" : (res.reason?.toLowerCase() === "advancedpaid" ? "Advance" : "Delivery")}
                                             </td>
                                         </tr>
                                     ))
@@ -173,46 +165,17 @@ export default function DailyStatusReport() {
                                         <td colSpan={headerFormat.dailyStatusReport.length} className="text-center text-danger">No Record Found For Payment</td>
                                     </tr>
                                 )}
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Orders Qty</td>
-                                    <td className="text-end">{totalBookingQty}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Amount</td>
-                                    <td className="text-end">{totalBookingAmount}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Advance Cash</td>
-                                    <td className="text-end">{totalBookingAdvanceCash}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Advance VISA</td>
-                                    <td className="text-end">{totalBookingAdvanceVisa}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Delivered Qty</td>
-                                    <td className="text-end">{totalDeliveredQty}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Delivery Cash</td>
-                                    <td className="text-end">{totalDeliveryCash}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Delivery VISA</td>
-                                    <td className="text-end">{totalDeliveryVisa}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Net Sale Amount</td>
-                                    <td className="text-end">{common.printDecimal(totalSalesAmount)}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Vat Tax {VAT}%</td>
-                                    <td className="text-end">{totalVatTax}</td>
-                                </tr>
-                                <tr style={{ fontSize: '12px' }}>
-                                    <td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Expenses</td>
-                                    <td className="text-end">{totalExpenseAmount}</td>
-                                </tr>
+                                {/* Aggregated Data Rows */}
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Orders Qty</td><td className="text-end">{totalBookingQty}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Amount</td><td className="text-end">{totalBookingAmount}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Advance Cash</td><td className="text-end">{totalAdvanceAmount('cash')}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Booking Advance VISA</td><td className="text-end">{totalAdvanceAmount('visa')}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Delivered Qty</td><td className="text-end">{totalDeliveredQty}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Delivery Cash</td><td className="text-end">{totalDeliveryAmount('cash')}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Delivery VISA</td><td className="text-end">{totalDeliveryAmount('visa')}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Net Sale Amount</td><td className="text-end">{common.printDecimal(totalSalesAmount)}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Vat Tax {VAT}%</td><td className="text-end">{totalVatTax}</td></tr>
+                                <tr><td colSpan={headerFormat.dailyStatusReport.length - 1} className="text-end">Total Expenses</td><td className="text-end">{totalExpenseAmount}</td></tr>
                             </tbody>
                         </table>
                     </div>
@@ -224,4 +187,3 @@ export default function DailyStatusReport() {
         </>
     );
 }
-
