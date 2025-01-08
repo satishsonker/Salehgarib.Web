7157fcb7f71eff3c9c76500d23b82dc35a2daf28
Satish Kumar Sonker <btech.csit@gmail.com>
2025-01-08 10:32:41 +0530

    app setting menu

--- src/components/common/LeftMenu.js
+++ src/components/common/LeftMenu.js
@@ -444,6 +444,9 @@ export default function LeftMenu({ setAuthData, authData, isSidebarCollapsed, se
                                                         </li>
                                                         <li>
                                                             <LeftMenuItem hasAccess={hasAccess} icon="bi bi-brightness-alt-low" menuName="Work Descriptipn" link="master-data/work-description" />
+                                                        </li> 
+                                                        <li>
+                                                            <LeftMenuItem hasAccess={hasAccess} icon="bi bi-brightness-alt-low" menuName="App Settings" link="application/settings" />
                                                         </li>
                                                         <li>
                                                             <LeftMenuItem hasAccess={hasAccess} icon="bi bi-broadcast-pin" menuName="Fabric Brand" link="fabric/master/brand" />
