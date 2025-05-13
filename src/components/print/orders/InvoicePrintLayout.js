53d355f55fc3e365a7ee94b449ab9a2347c8faf2
Satish Kumar Sonker <btech.csit@gmail.com>
2025-05-13 13:59:53 +0530

    Add Dirham symbol

--- src/components/print/orders/InvoicePrintLayout.js
+++ src/components/print/orders/InvoicePrintLayout.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { common } from '../../../utils/common'
 import ReceiptFooter from '../ReceiptFooter'
 import InvoiceHead from '../../common/InvoiceHead'
-import OrderCommonHeaderComponent from './OrderCommonHeaderComponent'
+import OrderCommonHeaderComponent from './OrderCommonHeaderComponent';
+import DirhamSymbol from '../../common/DirhamSymbol';
 
 export default function InvoicePrintLayout({ printRef, mainData, finalOrder }) {
     const vat = parseFloat(process.env.REACT_APP_VAT);
@@ -60,14 +61,14 @@ export default function InvoicePrintLayout({ printRef, mainData, finalOrder }) {
                                 <table className="table table-invoice" style={{ fontSize: '12px' }}>
                                     <thead>
                                         <tr>
-                                            <th className='text-center all-border'  width="5%" style={{ width: 'max-content !important' }}>S.No.</th>
-                                            <th className="text-center all-border" width="40%">DESCRIPTION</th>
-                                            <th className="text-center all-border" width="10%">Model No.</th>
+                                            <th className='text-center all-border'  width="1%" style={{ width: 'max-content !important' }}>S.</th>
+                                            <th className="text-center all-border"style={{paddingRight:'0',paddingLeft:'1px'}} width="25%">DESCRIPTION</th>
+                                            <th className="text-center all-border" width="1%">Model</th>
                                             <th className="text-center all-border" width="5%">Qty.</th>
                                             <th className="text-center all-border" width="10%">Item Price</th>
-                                            <th className="text-center all-border" width="5%">Unit Price</th>
-                                            <th className="text-center all-border" width="5%">Vat {vat}%</th>
-                                            <th className="text-center all-border" width="5%">Total Dhs.</th>
+                                            <th className="text-center all-border" width="10%">Unit Price</th>
+                                            <th className="text-center all-border" width="10%">Vat {vat}%</th>
+                                            <th className="text-center all-border" width="10%">Total Dhs.</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -75,14 +76,14 @@ export default function InvoicePrintLayout({ printRef, mainData, finalOrder }) {
                                         {
                                             finalOrder?.map((ele, index) => {
                                                 return <tr key={ele.id} className='print-table-height'>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="5%" style={{ width: 'max-content !important' }}>{hasKeys(ele) ? (index + 1) + "." : ""}</td>
-                                                    <td className={"text-center border border-secondary text-wrap" + getClassName(ele,index)} width="40%">{getWorkOrderTypes(ele.workType)}</td>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="5%">{getModel(ele)}</td>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="10%">{common.printDecimal(ele.qty, true)}</td>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="10%">{common.printDecimal((ele.subTotalAmount / ele.qty), true)}</td>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="5%">{common.printDecimal(ele.subTotalAmount, true)}</td>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="5%">{common.printDecimal(ele.vatAmount, true)}</td>
-                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="5%">{common.printDecimal(ele.totalAmount, true)}</td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="1%" style={{ width: 'max-content !important' }}>{hasKeys(ele) ? (index + 1) + "." : ""}</td>
+                                                    <td style={{paddingRight:'0',paddingLeft:'1px'}} className={"text-center border border-secondary text-wrap" + getClassName(ele,index)} width="25%">{getWorkOrderTypes(ele.workType)}</td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="1%">{getModel(ele)}</td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="5%">{common.printDecimal(ele.qty, true)}</td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="10%"><DirhamSymbol amount={common.printDecimal((ele.subTotalAmount / ele.qty), true)}/></td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="10%"><DirhamSymbol amount={common.printDecimal(ele.subTotalAmount, true)}/></td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="10%"><DirhamSymbol amount={common.printDecimal(ele.vatAmount, true)}/></td>
+                                                    <td className={"text-center border border-secondary" + getClassName(ele,index)} width="10%"><DirhamSymbol amount={common.printDecimal(ele.totalAmount, true)}/></td>
                                                 </tr>
                                             })
                                         }
@@ -96,25 +97,25 @@ export default function InvoicePrintLayout({ printRef, mainData, finalOrder }) {
                                             <td colSpan={1} className="text-end" >Total Quantity</td>
                                             <td colSpan={2} className="text-center">VAT {vat}%</td>
                                             <td colSpan={1} className="fs-6 fw-bold text-center">Total Amount</td>
-                                            <td colSpan={1} className="text-end">{common.printDecimal(mainData?.subTotalAmount)}</td>
+                                            <td colSpan={1} className="text-end"><DirhamSymbol amount={common.printDecimal(mainData?.subTotalAmount)}/></td>
                                         </tr>
                                         <tr>
                                             <td colSpan={3} className="text-start"><i className='bi bi-mail' /> {process.env.REACT_APP_COMPANY_EMAIL}<i className='bi bi-envelope text-success'></i></td>
                                             <td colSpan={1} className="text-center" >{mainData?.orderDetails?.filter(x => !x.isDeleted && !x.isCancelled)?.length}</td>
                                             <td className="fs-6 fw-bold text-center">Total VAT</td>
-                                            <td className="text-end">{common.printDecimal(totalVat)}</td>
+                                            <td className="text-end"><DirhamSymbol amount={common.printDecimal(totalVat)}/></td>
                                             <td className="fs-6 fw-bold text-center">Gross Amount</td>
-                                            <td className="text-end">{common.printDecimal((mainData?.totalAmount - cancelledOrDeletedTotal))}</td>
+                                            <td className="text-end"><DirhamSymbol amount={common.printDecimal((mainData?.totalAmount - cancelledOrDeletedTotal))}/></td>
                                         </tr>
                                         <tr>
                                             <td colSpan={6} className="text-start">Received by.................................</td>
                                             <td className="fs-6 fw-bold text-center">Total Advance</td>
-                                            <td className="text-end">{common.printDecimal(mainData?.accountStatements?.find(x => x.isFirstAdvance)?.credit ?? 0)}</td>
+                                            <td className="text-end"><DirhamSymbol amount={common.printDecimal(mainData?.accountStatements?.find(x => x.isFirstAdvance)?.credit ?? 0)}/></td>
                                         </tr>
                                         <tr>
                                             <td colSpan={6} className="text-start"></td>
                                             <td className="fs-6 fw-bold text-center">Total Balance</td>
-                                            <td className="text-end">{common.printDecimal(mainData?.totalAmount - cancelledOrDeletedTotal - (mainData?.accountStatements?.find(x => x.isFirstAdvance)?.credit ?? 0))}</td>
+                                            <td className="text-end"><DirhamSymbol amount={common.printDecimal(mainData?.totalAmount - cancelledOrDeletedTotal - (mainData?.accountStatements?.find(x => x.isFirstAdvance)?.credit ?? 0))}/></td>
                                         </tr>
                                     </tbody>
                                 </table>
